3ced25760c6daf3e4cf5c300ecf3946977b67655
Chandana Sudha Madhuri Kandari <madhurikandari5@gmail.com>
2024-04-03 20:26:38 +0530

    Initial Commit

---
+++ TransactionDetails/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TransactionDetails
+//
+//  Created by Chandana Sudha Madhuri Kandari on 03/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TransactionDetails/TransactionDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionDetailsApp.swift
+//  TransactionDetails
+//
+//  Created by Chandana Sudha Madhuri Kandari on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TransactionDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
